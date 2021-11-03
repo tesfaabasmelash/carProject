8b09cc8b8753495d27d346a1650ff7a75f61607e
tesfaabasmelash <asmelashtesfaab@gmail.com>
2021-11-03 18:28:04 -0500

    The basic tracture of the car

---
+++ src/gitstash/Car.java
@@ -0,0 +1,11 @@
+package gitstash;
+
+public class Car {
+    public String color;
+    public String model;
+    public int doors;
+    public String name;
+    public String engineCapacity;
+
+
+}
---
+++ src/gitstash/Main.java
@@ -0,0 +1,9 @@
+package gitstash;
+
+public class Main {
+    public static void main(String[] args) {
+        Car newcar = new Car();
+
+
+    }
+}
