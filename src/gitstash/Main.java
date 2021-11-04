3fd76b0e6c1420494762b9fbeaba8554734c12de
tesfaabasmelash <asmelashtesfaab@gmail.com>
2021-11-03 19:14:34 -0500

    The blue Ferrari car better than the Lambo I created in stashing, If you want to see that let me know

--- src/gitstash/Main.java
+++ src/gitstash/Main.java
@@ -3,7 +3,12 @@ package gitstash;
 public class Main {
     public static void main(String[] args) {
         Car newcar = new Car();
+        newcar.name = "Feerarriii";
+        newcar.color = "Blue";
+        newcar.doors = 3;
+        newcar.engineCapacity = "60000cc";
 
+        System.out.println(newcar.color);
 
     }
 }
